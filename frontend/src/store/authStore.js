8d56285ed3cebd929a1257ec87d83aa32bf8016e
Devazharruk <azharruk66@gmail.com>
2024-09-14 02:43:32 -0700

    commit

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
+const API_URL = "https://mern-authapi.vercel.app/api/auth"
 
 axios.defaults.withCredentials = true;
 
