5050f47f653601e98653a13ed5680e6284b1c877
Azhar Ali <149366630+Devazharruk@users.noreply.github.com>
2024-09-14 03:25:07 -0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(cors({ origin: "https://mern-auth-ochre.vercel.app/", credentials: true }));
+app.use(cors({ origin: "https://mern-auth-ochre.vercel.app", credentials: true }));
 
 app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
